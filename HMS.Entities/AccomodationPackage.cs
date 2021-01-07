8d7bef791f033b9dd1c888623307d99e2274fd7f
GeethanjaliSangu <geethanjalijawahar0@gmail.com>
2021-01-07 08:10:41 -0800

    Created Basic Entitites

---
+++ HMS.Entities/AccomodationPackage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMS.Entities
+{
+    public class AccomodationPackage
+    {
+        public int Id { get; set; }
+        public int AccomodationTypeId { get; set; }
+        public AccomodationType AccomodationType { get; set; }
+        public string Name { get; set; }
+        public int NoOfRooms { get; set; }
+        public decimal FeePerNight { get; set; }
+    }
+}
---
+++ HMS.Entities/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HMS.Entities
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        public int AccomodationId { get; set; }
+
+        public Accomodation Accomodation { get; set; }
+        public DateTime FromDate { get; set; }
+
+        public int Duration { get; set; }
+    }
+}
